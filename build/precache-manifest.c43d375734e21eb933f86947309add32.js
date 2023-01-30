3daab7959d19d2debc86b51c14aa46502ff966e7
Jamal Khan <ahtishamkayani01@gmail.com>
2023-01-30 22:10:29 +0500

    build

---
+++ build/precache-manifest.c43d375734e21eb933f86947309add32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8c33f82694dfde11037",
+    "url": "/static/css/main.84343690.chunk.css"
+  },
+  {
+    "revision": "c8c33f82694dfde11037",
+    "url": "/static/js/main.c8c33f82.chunk.js"
+  },
+  {
+    "revision": "fbbb520a3b15bb226ee6",
+    "url": "/static/js/1.fbbb520a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9127be796c8c9edd001a719e44e452de",
+    "url": "/index.html"
+  }
+];
